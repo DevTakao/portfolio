6fbacb05f933454465eef39df06213ead80238fe
TK <developer.aungkaungkhant@gmail.com>
2023-12-27 20:57:23 +0630

    feat: audio toggle and rounded footer

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <motion.div
       {...fadeInAnimation}
-      className="relative z-10 max-w-screen-md mx-auto mt-10 py-5 px-6 text-center text-base font-serif bg-light roundedmotion.-xl shadow-2xl"
+      className="relative z-10 max-w-screen-md mt-10 py-5 px-6 text-center text-base font-serif bg-light mx-7 sm:mx-auto rounded-xl shadow-2xl"
     >
       &copy; Copyright, Aung Kaung Khant, 2023, All rights reserved.
     </motion.div>
--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -4,11 +4,19 @@ import Footer from "../common/Footer";
 import Music from "../assets/sounds/down_by_the_river_nerissa.mp3";
 import ReactAudioPlayer from "react-audio-player";
 import { useAppStore } from "../store/appStore";
+import { FaVolumeUp, FaVolumeMute } from "react-icons/fa";
 
 const PortfolioPage = () => {
-  const { isAudioOn } = useAppStore();
+  const { isAudioOn, setAudio } = useAppStore();
+  const toggleAudio = () => setAudio(!isAudioOn);
   return (
     <div className="w-full py-8 bg-[#f1f1f1] text-dark text-center">
+      <button
+        onClick={toggleAudio}
+        className="absolute top-0 right-5 p-3 inline-flex items-center justify-center bg-light shadow-xl z-50 rounded-b-xl"
+      >
+        {isAudioOn ? <FaVolumeUp className="animate-pulse" /> : <FaVolumeMute />}
+      </button>
       <PortfolioHeader />
       <PortfolioContent />
       <Footer />
