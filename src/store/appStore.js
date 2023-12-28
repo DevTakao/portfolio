083732e09bb7d39badc0c9afcf555f971afd10e7
TK <developer.aungkaungkhant@gmail.com>
2023-12-28 22:09:44 +0630

    chore: re-add start page sequence

--- src/store/appStore.js
+++ src/store/appStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 const defaultState = {
-  isStartClicked: true,
+  isStartClicked: false,
   isAudioOn: false,
   isMobileNavOpen: false,
   currentMain: "profile",
