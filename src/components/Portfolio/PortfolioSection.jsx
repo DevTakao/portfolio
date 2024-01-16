7fea356bcb44fb250dd5ea276cdd1fe800913e53
TK <developer.aungkaungkhant@gmail.com>
2024-01-16 17:53:30 +0630

    fix: bg cover and no repeat

--- src/components/Portfolio/PortfolioSection.jsx
+++ src/components/Portfolio/PortfolioSection.jsx
@@ -9,7 +9,7 @@ const PortfolioSection = () => {
         style={{
           backgroundImage: `url(${BG})`,
         }}
-        className="Background w-full h-full absolute top-0 left-0 bg-fixed bg-auto bg-center z-0 backdrop-grayscale"
+        className="Background w-full h-full absolute top-0 left-0 bg-fixed bg-cover bg-no-repeat bg-center z-0 backdrop-grayscale"
       />
 
       <div className="Content w-full py-8 bg-[#f1f1f1] text-dark text-center">
