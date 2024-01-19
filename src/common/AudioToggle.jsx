f7b1bcb5ec91e50bfdb2035b0a3ef7c98ecc131b
TK <developer.aungkaungkhant@gmail.com>
2024-01-19 23:34:45 +0630

    fix: reuse react audio player

--- src/common/AudioToggle.jsx
+++ src/common/AudioToggle.jsx
@@ -1,43 +1,31 @@
-import { useEffect, useRef, useState } from "react";
+import { FaVolumeUp, FaVolumeMute } from "react-icons/fa";
+import { useEffect, useState } from "react";
 import { useAppStore } from "@store/appStore";
-import AudioVisual from "react-audio-visual";
+import ReactAudioPlayer from "react-audio-player";
 import Music from "@assets/sounds/Timecop1983_On_the_Run.mp3";
 
 const AudioToggle = () => {
   const { isAudioOn, setAudio } = useAppStore();
   const [musicSrc, setMusicSrc] = useState(null);
-  const audioRef = useRef();
   const toggleAudio = () => setAudio(!isAudioOn);
 
   useEffect(() => {
-    const ref = audioRef.current;
     setMusicSrc(Music);
-    if (ref) {
-      if (isAudioOn) {
-        ref.muted = false;
-      } else {
-        ref.muted = false;
-      }
-    }
 
     return () => {
       setMusicSrc(null);
-      ref.muted = true;
     };
-  }, [isAudioOn]);
+  }, []);
 
   return (
     <>
       <button
         onClick={toggleAudio}
-        className="fixed top-0 right-5 p-3 inline-flex items-center justify-center bg-yellow border-b border-x border-white z-[999] rounded-b-xl"
-        style={{ boxShadow: "0px 0px 10px 3px #a4fff6" }}
+        className="fixed top-0 right-5 p-3 inline-flex items-center justify-center bg-light shadow-xl z-[999] rounded-b-xl"
       >
-        <div className="w-6 h-6">
-          <AudioVisual audio={audioRef} />
-        </div>
+        {isAudioOn ? <FaVolumeUp className="animate-pulse" /> : <FaVolumeMute />}
       </button>
-      <audio ref={audioRef} src={musicSrc} autoPlay={false} loop />
+      <ReactAudioPlayer src={musicSrc} autoPlay={true} volume={0.75} muted={!isAudioOn} loop />
     </>
   );
 };
