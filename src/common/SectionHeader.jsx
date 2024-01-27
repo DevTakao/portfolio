72db8e94841e3f80fd221d4c9c18965baaec1a56
TK <developer.aungkaungkhant@gmail.com>
2024-01-28 00:35:44 +0630

    style: reduce mobile section spacing

--- src/common/SectionHeader.jsx
+++ src/common/SectionHeader.jsx
@@ -1,5 +1,5 @@
 const SectionHeader = ({ children }) => {
-  return <h2 className="text-center text-4xl font-medium mb-24">{children}</h2>;
+  return <h2 className="text-center text-4xl font-medium mb-12 md:mb-24">{children}</h2>;
 };
 
 export default SectionHeader;
