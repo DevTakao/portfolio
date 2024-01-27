ca2d3f2997c806a1109fc25aac58c380364d1c14
TK <developer.aungkaungkhant@gmail.com>
2024-01-28 00:34:53 +0630

    feat: slider  reflection effect, center cards vertically

--- src/components/Skills/SkillsSection.jsx
+++ src/components/Skills/SkillsSection.jsx
@@ -14,38 +14,49 @@ const SkillsSection = () => {
   const [activeId, setActiveId] = useState(0);
 
   return (
-    <section className="Skills_Section section-container px-1">
-      <SectionHeader>My Weapons</SectionHeader>
-      <div className="select-none">
-        <Swiper
-          modules={[Mousewheel]}
-          mousewheel
-          grabCursor
-          centeredSlides
-          slideToClickedSlide
-          slidesPerView="auto"
-          onSlideChange={({ realIndex }) => setActiveId(realIndex)}
-          style={{ padding: "20px 0 55px 0" }}
-        >
-          {skills.map((skill, i) => (
-            <SwiperSlide key={i} style={{ maxWidth: "300px", padding: "0 15px" }}>
-              <div className="w-full h-[35vh] flex items-center justify-center">
-                <SkillCard skill={skill} />
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-      <div className="Description min-h-[2rem] px-10">
+    <section className="Skills_Section section-container px-1 relative overflow-hidden">
+      <div className="Background absolute z-[0] w-full h-full top-0 left-0 blur-md flex items-center justify-center">
         <AnimatePresence mode="popLayout">
-          <motion.div
-            key={activeId}
+          <motion.img
             {...fadeInAnim}
-            className="max-w-screen-lg mx-auto text-lg text-justify md:text-center"
-          >
-            {skills[activeId]?.description || ""}
-          </motion.div>
+            key={activeId}
+            src={skills[activeId].logo}
+            className="w-[90%] h-[90%] object-contain translate-y-[25%]"
+          />
         </AnimatePresence>
+      </div>
+      <div className="Backdrop absolute z-[1] w-full h-full top-0 left-0 bg-light bg-opacity-60" />
+      <div className="Main relative z-[2]">
+        <SectionHeader>My Weapons</SectionHeader>
+        <div className="select-none">
+          <Swiper
+            modules={[Mousewheel]}
+            mousewheel
+            grabCursor
+            centeredSlides
+            slideToClickedSlide
+            slidesPerView="auto"
+            onSlideChange={({ realIndex }) => setActiveId(realIndex)}
+            style={{ padding: "20px 0 55px 0" }}
+          >
+            {skills.map((skill, i) => (
+              <SwiperSlide key={i} style={{ maxWidth: "300px", padding: "0 15px" }}>
+                <SkillCard skill={skill} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div className="Description min-h-[2rem] px-10">
+          <AnimatePresence mode="wait">
+            <motion.div
+              key={activeId}
+              {...fadeInAnim}
+              className="max-w-screen-lg mx-auto text-lg text-justify md:text-center"
+            >
+              {skills[activeId]?.description || ""}
+            </motion.div>
+          </AnimatePresence>
+        </div>
       </div>
     </section>
   );
