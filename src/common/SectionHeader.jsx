8a52e081d28b2e6e6429e962c7e70d795974b3e2
TK <developer.aungkaungkhant@gmail.com>
2024-01-29 23:26:50 +0630

    fix: layout shift during animation

--- src/common/SectionHeader.jsx
+++ src/common/SectionHeader.jsx
@@ -19,14 +19,14 @@ const SectionHeader = ({ children }) => {
   const containerRef = useRef(null);
   const inView = useInView(containerRef);
   return (
-    <div ref={containerRef} className="overflow-hidden mb-12 md:mb-24 py-1">
+    <div ref={containerRef} className="overflow-hidden h-12 mb-12 md:mb-24 relative">
       <AnimatePresence>
         {inView && (
           <motion.h2
             variants={variants}
             initial="initial"
             animate="animate"
-            className="text-center text-5xl font-medium"
+            className="text-center w-full text-5xl font-medium absolute top-0 left-0"
           >
             {children}
           </motion.h2>
