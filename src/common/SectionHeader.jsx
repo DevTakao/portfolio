6168ee19ef739c73abf39f83ce3df84804a9d7ad
TK <developer.aungkaungkhant@gmail.com>
2024-01-29 23:20:22 +0630

    feat: section header animation

--- src/common/SectionHeader.jsx
+++ src/common/SectionHeader.jsx
@@ -1,5 +1,39 @@
+import { useRef } from "react";
+import { AnimatePresence, motion, useInView } from "framer-motion";
+
+const variants = {
+  initial: {
+    y: "99%",
+    opacity: 0.001,
+  },
+  animate: {
+    y: 0,
+    opacity: 0.999,
+    transition: {
+      duration: 0.7,
+    },
+  },
+};
+
 const SectionHeader = ({ children }) => {
-  return <h2 className="text-center text-4xl font-medium mb-12 md:mb-24">{children}</h2>;
+  const containerRef = useRef(null);
+  const inView = useInView(containerRef);
+  return (
+    <div ref={containerRef} className="overflow-hidden mb-12 md:mb-24 py-1">
+      <AnimatePresence>
+        {inView && (
+          <motion.h2
+            variants={variants}
+            initial="initial"
+            animate="animate"
+            className="text-center text-5xl font-medium"
+          >
+            {children}
+          </motion.h2>
+        )}
+      </AnimatePresence>
+    </div>
+  );
 };
 
 export default SectionHeader;
