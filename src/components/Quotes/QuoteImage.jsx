7327507078b875e9f4bc614bcf6a9be2c65f4947
TK <developer.aungkaungkhant@gmail.com>
2024-02-08 01:13:29 +0630

    feat: quote character bg

--- src/components/Quotes/QuoteImage.jsx
+++ src/components/Quotes/QuoteImage.jsx
@@ -1,6 +1,7 @@
 import Loader from "@common/Loader";
 import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
+import QuoteBG from "@assets/images/quote-bg.svg";
 
 const fadeVariants = {
   initial: {
@@ -22,6 +23,34 @@ const fadeVariants = {
   },
 };
 
+const bgArtVariants = {
+  initial: {
+    x: "-50%",
+    y: "-50%",
+    scale: 0,
+    rotate: 0,
+  },
+  animate: {
+    x: "-50%",
+    y: "-50%",
+    scale: 1.25,
+    rotate: 55,
+    transition: {
+      duration: 0.75,
+      ease: [0.2, 0.3, 0.8, 1],
+    },
+  },
+  exit: {
+    x: "-50%",
+    y: "-50%",
+    opacity: 0,
+    transition: {
+      duration: 0.5,
+      ease: [0.2, 0.7, 0.9, 1],
+    },
+  },
+};
+
 const QuoteImage = ({ page, data }) => {
   const [loaded, setLoaded] = useState(false);
 
@@ -38,6 +67,17 @@ const QuoteImage = ({ page, data }) => {
       </div>
       <AnimatePresence mode="wait">
         <motion.img
+          key={page}
+          variants={bgArtVariants}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          src={QuoteBG}
+          className="absolute z-[0] top-[50%] left-[50%] w-full h-full object-contain"
+        />
+      </AnimatePresence>
+      <AnimatePresence mode="wait">
+        <motion.img
           onLoad={() => setLoaded(true)}
           key={page}
           variants={fadeVariants}
@@ -47,7 +87,7 @@ const QuoteImage = ({ page, data }) => {
           src={data[page]?.image}
           alt={data[page]?.alt}
           style={{ display: loaded ? "block" : "none" }}
-          className="w-full h-full object-contain absolute inset-0"
+          className="w-full h-full object-contain absolute inset-0 z-[1]"
         />
       </AnimatePresence>
     </div>
